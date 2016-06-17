5c27c4da9effcf51d0f31e368ccc5dbe7b88f794
Taylor Yee <sonictjy@hotmail.com>
2016-06-17 16:58:43 -0700

    Initial Commit
    Initial commit of work.

---
+++ MenuTestingProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(int level)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar(int level)
+    {
+        async = SceneManager.LoadSceneAsync(level);
+        while (!async.isDone)
+        {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ MenuTestingProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public AudioClip pickUpSound;
+    public AudioClip VictoryJingle;
+    public GameObject quitButton;
+
+    private Rigidbody rb;
+    private int count;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+	
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVericatl = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVericatl);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pick Up")
+        {
+            other.gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(pickUpSound, transform.position);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13)
+        {
+            winText.text = "You Win!";
+            AudioSource.PlayClipAtPoint(VictoryJingle, transform.position);
+            quitButton.SetActive(true);
+        }
+    }
+}
